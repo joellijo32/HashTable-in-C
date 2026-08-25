fd922f7292f6e2ee2680d1ff353e7cb56f4f8633
Joel Lijo Mathew <joellijo4@gmail.com>
2026-08-25 12:05:06 +0530

    Signature mismatch in header and c file

--- src/hash_table.h
+++ src/hash_table.h
@@ -21,7 +21,7 @@ typedef struct {
 ht_hash_table* ht_new(void);
 void ht_del_hash_table(ht_hash_table* ht);
 void ht_insert(ht_hash_table* ht, const char* key, const char* value);
-char* ht_search(ht_hash_table* ht, const char* key);
+char* ht_search(const ht_hash_table* ht, const char* key);
 void ht_delete(ht_hash_table* ht, const char* key);
 
 #endif
