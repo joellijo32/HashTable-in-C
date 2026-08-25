86b737dcfb91729b89c6b40cd8f05925bd0dc2a5
Joel Lijo Mathew <joellijo4@gmail.com>
2026-08-25 10:07:59 +0530

    Added include guards and prototypes to header file

--- src/hash_table.h
+++ src/hash_table.h
@@ -1,3 +1,6 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
 typedef struct {
 	char* key;
 	char* value;
@@ -10,4 +13,11 @@ typedef struct {
 	ht_item** items;
 } ht_hash_table;
 
+ht_hash_table* ht_new(void);
+void ht_del_hash_table(ht_hash_table* ht);
+void ht_insert(ht_hash_table* ht, const char* key, const char* value);
+char* ht_search(ht_hash_table* ht, const char* key);
+void ht_delete(ht_hash_table* ht, const char* key);
+
+#endif
 
