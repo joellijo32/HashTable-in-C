cfff717cdc748154fd91b0f830e777cec56935e2
Joel Lijo Mathew <joellijo4@gmail.com>
2026-08-25 11:50:42 +0530

    second hashing range reduced for infinte loop avoidance

--- src/hash_table.c
+++ src/hash_table.c
@@ -69,7 +69,7 @@ static int ht_get_hash(
 		const char* s, const int num_buckets, const int attempt
 ) {
 	const int hash_a = ht_hash(s, HT_PRIME_1, num_buckets);
-	const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
+	const int hash_b = 1 + (ht_hash(s, HT_PRIME_2, num_buckets-1));
 	return (hash_a + (attempt*(hash_b+1))) % num_buckets;
 }
 
